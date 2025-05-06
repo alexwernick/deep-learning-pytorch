9716b3332ab757bd501dea10cf763cf8d6562998
Alex Wernick <alex.j.wernick@gmail.com>
2025-05-06 15:18:34 +0100

    one hot ending

---
+++ deep_learning_pytorch/cross_entropy_loss.py
@@ -0,0 +1,9 @@
+from torch.nn import CrossEntropyLoss
+import torch
+
+# scores - model predictions before the final softmax function 
+scores = torch.tensor([-5.2, 4.6, 0.8])
+one_hot_target = torch.tensor([1, 0, 0])
+
+criterion = CrossEntropyLoss()
+print(criterion(scores.double(), one_hot_target.double()))
